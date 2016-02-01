97ea4e5580858185ebc2886d526ca448cb189a02
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-01 13:10:06 -0500

    Creates FISBlackjackGame and FISBlackjackPlayer and stubs out methods.

---
+++ BlackJack/FISBlackjackGame.m
@@ -0,0 +1,53 @@
+//
+//  FISBlackjackGame.m
+//  BlackJack
+//
+//  Created by Ken M. Haggerty on 2/1/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#import "FISBlackjackGame.h"
+
+@implementation FISBlackjackGame
+
+- (void)playBlackjack {
+    
+    //
+}
+
+- (void)dealNewRound {
+    
+    //
+}
+
+- (void)dealCardToPlayer {
+    
+    //
+}
+
+- (void)dealCardToHouse {
+    
+    //
+}
+
+- (void)processPlayerTurn {
+    
+    //
+}
+
+- (void)processHouseTurn {
+    
+    //
+}
+
+- (BOOL)houseWins {
+    
+    return NO;
+}
+
+- (void)incrementWinsAndLossesForHouseWins:(BOOL)houseWins {
+    
+    //
+}
+
+@end
---
+++ BlackJack/FISBlackjackPlayer.m
@@ -0,0 +1,34 @@
+//
+//  FISBlackjackPlayer.m
+//  BlackJack
+//
+//  Created by Ken M. Haggerty on 2/1/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#import "FISBlackjackPlayer.h"
+
+@implementation FISBlackjackPlayer
+
+- (instancetype)initWithName:(NSString *)name {
+    
+    self = [super init];
+    return self;
+}
+
+- (void)resetForNewGame {
+    
+    //
+}
+
+- (void)acceptCard:(FISCard *)card {
+    
+    //
+}
+
+- (BOOL)shouldHit {
+    
+    return NO;
+}
+
+@end
